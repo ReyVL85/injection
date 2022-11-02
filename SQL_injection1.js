df46396475cba3729d293b2033dff91a5e4fdec2
ReyVL85 <97484055+ReyVL85@users.noreply.github.com>
2022-11-02 19:15:35 +0100

    Delete SQL_injection1.js

--- SQL_injection1.js
+++
@@ -1,7 +0,0 @@
-// SQL_injection.js
-router.get('/example3/user/:id', (req,res) => {
-let userId = req.params.id;
-connection.query({
-sql : "SELECT * FROM users WHERE id=" +userId
-},(err, result) => {
-res.json(result);
